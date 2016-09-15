8146181b1e3a62663cd69c5b082b5d3d6796892c
Ronny Almog <ronny2205@gmail.com>
2016-09-15 10:58:08 +0000

    adding the customer to the db

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,17 @@
 class CustomersController < ApplicationController
   def new
     @customer = Customer.new
+  end  
+
+  def create
+    Customer.create(customer_params)
+    redirect_to root_path
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :email, :password, :project_name, :org_name)
+  end
+  
 end
